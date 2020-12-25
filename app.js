a119e5d3cf8634c79bce443b55a4e2c4bf1b3bad
Abdul-Faiz <53165415+AbdulFaiz123@users.noreply.github.com>
2020-12-25 23:01:27 +0530

    feat

---
+++ app.js
@@ -0,0 +1,30 @@
+
+
+var button_primary = document.querySelector("#btn-primary")
+var txt_input = document.querySelector("#text-input")
+var txt_output = document.querySelector("#output")
+
+var serverUrl = "https://api.funtranslations.com/translate/minion.json"
+
+function Translator(text){
+    return serverUrl + "?"+"text="+text
+}
+function errorHandler(error){
+    console.log("error occured",error)
+    alert("Something Went Wrong with server! Trey again after some time")
+}
+
+function clickEventHandler(){
+    var Input_Text = txt_input.value
+
+    fetch(Translator(Input_Text))
+    .then(response => response.json())
+    
+    .then(json =>{
+        var TranslateTextOutput = json.contents.translated
+        txt_output.innerText = TranslateTextOutput;
+    })
+    .catch(errorHandler)
+}
+button_primary.addEventListener("click",clickEventHandler)
+
