7c10c2bcbda3facfba29189ad2312ce40458e0ae
Abdul-Faiz <53165415+AbdulFaiz123@users.noreply.github.com>
2021-09-28 16:24:56 +0530

    feat

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
 
 
 var button_primary_german = document.querySelector("#btn-primary-german")
-// var button_primary_minion = document.querySelector("#btn-primary-minion")
+
 var txt_input = document.querySelector("#translate-input")
 var txt_output = document.querySelector("#translate-output")
 
-// var serverUrlMinion = "https://api.funtranslations.com/translate/minion.json"
+
 var serverurlGerman ="https://api.funtranslations.com/translate/german-accent.json"
 
 function Translator(text){
     return serverurlGerman + "?"+"text="+text
 }
 
-    // button_primary_german
-    //     return serverurlGerman + "?"+"text="+text
-    // }
-//     else if(button_primary_minion){
-//         return serverUrlMinion + "?"+"text="+text
 
-//     }
 function errorHandler(error){
     console.log("error occured",error)
     alert("Something Went Wrong with server! Trey again after some time")
@@ -37,4 +31,4 @@ function clickEventHandler(){
     .catch(errorHandler)
 }
 button_primary_german.addEventListener("click",clickEventHandler)
-// button_primary_minion.addEventListener("click",clickEventHandler)
+
